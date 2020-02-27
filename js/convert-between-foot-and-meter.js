236bd9d320ca6131c2751084f0f49c0cdf9f4f4d
anhtu6 <anhtu632@gmail.com>
2020-02-27 10:20:29 +0700

    convert between foot and meter

---
+++ js/convert-between-foot-and-meter.js
@@ -0,0 +1,8 @@
+function convertFM(value1) {
+    value1 = parseFloat(value1);
+    document.getElementById('td1').innerText =value1*0.3;
+}
+function convertMF(value2) {
+    value2 = parseFloat(value2);
+    document.getElementById('td2').innerText =value2*3;
+}
